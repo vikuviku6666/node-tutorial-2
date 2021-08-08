82b5e092b41b3cc096133933921e5a69ba7192b5
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2021-08-08 19:59:57 +0200

    env file added

--- demo.js
+++ demo.js
@@ -1,29 +1,33 @@
+require('dotenv').config();
+
 const { MongoClient } = require('mongodb');
 
 async function main() {
-	const url = "mongodb+srv://vin6666:node123@cluster0.jjmb1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+	const url =
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjmb1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+	console.log(url);
 	const client = new MongoClient(url);
-try {
-	await client.connect();
+	try {
+		await client.connect();
 
-	await deleteListingsScrapedBeforeDate(client, new Date("2019-02-15"));
+		//await deleteListingsScrapedBeforeDate(client, new Date('2019-02-15'));
 
-	//await deleteListingByName(client, "Cozy Cottage");
+		//await deleteListingByName(client, "Cozy Cottage");
 
-	//await updateAllListingsToHavePropertyType(client);
+		//await updateAllListingsToHavePropertyType(client);
 
-	//await upsertListingByName(client, "Cozy Cottage", { name: "Cozy Cottage", bedrooms: 2, bathrooms: 2, price: 20000, reviews: [{ rating: 5, review: "I love this place" }] });
+		//await upsertListingByName(client, "Cozy Cottage", { name: "Cozy Cottage", bedrooms: 2, bathrooms: 2, price: 20000, reviews: [{ rating: 5, review: "I love this place" }] });
 
-/* 	await updateListingsByName(client, "Lovely Loft", {	bedrooms: 6, beds: 8}); */
+		/* 	await updateListingsByName(client, "Lovely Loft", {	bedrooms: 6, beds: 8}); */
 
-/* 	await findListingsWithMinimumBedroomsAndBathroomsAndMostRecentReviews(client, {
+		/* 	await findListingsWithMinimumBedroomsAndBathroomsAndMostRecentReviews(client, {
 		minimumNumberOfBedrooms: 2,
 		minimumNumberOfBathrooms: 2,
 		maximumNumberOfResults: 5
 	}); */
 
-	//await findOneListingByName(client, "Lovely Loft");
-/* 	await createMultipleListings(client, [
+		await findOneListingByName(client, "Lovely Loft");
+		/* 	await createMultipleListings(client, [
 		{
 			name: 'Lovely Loft',
 			summary: 'A charming loft in Paris',
@@ -43,7 +47,7 @@ try {
 			bathrooms: 1,
 		},
 	]); */
-	//await listDatabases(client);
+		//await listDatabases(client);
 	} catch (err) {
 		console.error(err);
 	} finally {
@@ -59,7 +63,7 @@ async function deleteListingsScrapedBeforeDate(client, date) {
 	const result = await client
 		.db('sample_airbnb')
 		.collection('listingsAndReviews')
-		.deleteMany({ "last_scraped": { $lt: date} });
+		.deleteMany({ last_scraped: { $lt: date } });
 	console.log(`${result.deletedCount} documents was/were deleted`);
 }
 
@@ -108,26 +112,31 @@ async function updateListingsByName(client, nameOfListing, updatedListing) {
 
 // read documents from a collection
 
-async function findListingsWithMinimumBedroomsAndBathroomsAndMostRecentReviews(client, {
-	minimumNumberOfBedrooms = 0,
-	minimumNumberOfBathrooms = 0,
-	maximumNumberOfResults = Number.MAX_SAFE_INTEGER,
-} = {}) {
+async function findListingsWithMinimumBedroomsAndBathroomsAndMostRecentReviews(
+	client,
+	{
+		minimumNumberOfBedrooms = 0,
+		minimumNumberOfBathrooms = 0,
+		maximumNumberOfResults = Number.MAX_SAFE_INTEGER,
+	} = {}
+) {
 	const cursor = await client
 		.db('sample_airbnb')
 		.collection('listingsAndReviews')
 		.find({
 			bedrooms: { $gte: minimumNumberOfBedrooms },
-			bathrooms: { $gte: minimumNumberOfBathrooms }
+			bathrooms: { $gte: minimumNumberOfBathrooms },
 		})
 		.sort({
-			'last_review': -1,
+			last_review: -1,
 		})
 		.limit(maximumNumberOfResults);
-	
+
 	const results = await cursor.toArray();
 	if (results.length > 0) {
-		console.log(`Found ${results.length} listings with ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`);
+		console.log(
+			`Found ${results.length} listings with ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`
+		);
 		results.forEach((result, i) => {
 			date = new Date(result.last_review).toDateString();
 			console.log();
@@ -138,12 +147,17 @@ async function findListingsWithMinimumBedroomsAndBathroomsAndMostRecentReviews(c
 			console.log(`-recent_review: ${new Date(result.last_review).toDateString()}`);
 		});
 	} else {
-		console.log(`listings with ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`);
-		}
+		console.log(
+			`listings with ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms`
+		);
+	}
 }
 
 async function findOneListingByName(client, nameOfListing) {
-	const result =  await client.db("sample_airbnb").collection("listingsAndReviews").findOne({ name: nameOfListing });
+	const result = await client
+		.db('sample_airbnb')
+		.collection('listingsAndReviews')
+		.findOne({ name: nameOfListing });
 
 	if (result) {
 		console.log(`Found a ${nameOfListing}`);
@@ -172,9 +186,9 @@ async function createListing(client, newListing) {
 
 async function listDatabases(client) {
 	const databasesList = await client.db().admin().listDatabases();
-	
-	console.log("Databases:");
-	databasesList.databases.forEach(db => {
+
+	console.log('Databases:');
+	databasesList.databases.forEach((db) => {
 		console.log(`- ${db.name}`);
 	});
 }
